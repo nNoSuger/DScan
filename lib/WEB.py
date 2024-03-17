40685fc1cb86d0c0e850a868bd7394f54bbdbef9
nNoSuger <130155594+nNoSuger@users.noreply.github.com>
2024-03-17 14:15:05 +0800

    Update WEB.py

--- lib/WEB.py
+++ lib/WEB.py
@@ -17,9 +17,7 @@ class WEB_DIR(threading.Thread):
                 response = requests.get(url, headers=head, timeout=1)
                 if response.status_code in [200, 302, 403]:
                     print('[*]' + url + ' status[' + str(response.status_code) + ']')
-                    # 使用当前命令行参数传入的域名命名文件
                     file_name = f"{self._domain}.txt"
-                    # 保存内容到以当前命令行参数传入的域名命名的文件中
                     with open(file_name, 'a') as file:
                         file.write('[*]' + url + ' status[' + str(response.status_code) + ']\n')
             except:
